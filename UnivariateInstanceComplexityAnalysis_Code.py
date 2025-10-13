9f301933b64291eb6728ae990fb0d8a078ff6886
carmenlancho <carmenlancho@hotmail.com>
2025-10-13 13:18:37 -0600

    Script con códigos para analizar relación entre complejidades de instancias obtenidas univariantemente por las variables

---
+++ UnivariateInstanceComplexityAnalysis_Code.py
@@ -0,0 +1,14 @@
+###################################################################################################################
+######                 COMPLEJIDAD INSTANCIAS ARROJADA POR CADA VARIABLE DE FORMA UNIVARIANTE                ######
+###################################################################################################################
+
+# En este script vamos a poner el código que nos permite analizar la relación entre las complejidades
+# a nivel instancia que arrojan las distintas variables de forma univariante
+# La idea es estudiar si, variables que están relacionadas entre sí, otorgan complejidades similares
+# de modo que se podría hacer un filtro previo de dichas variables redundantes
+# Es un poco seguir la idea de eliminar variables con alta correlación pero en base a las complejidades
+# En este script vamos a poner las funciones necesarias para analizar los resultados
+# y en el notebook UnivariateInstanceComplexity_Analysis.ipnyb ponemos los resultados ya más visuales
+
+
+
