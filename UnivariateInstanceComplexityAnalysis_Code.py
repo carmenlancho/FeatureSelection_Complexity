f541e55dfd78953a4bffd4979d9da66798e6204c
carmenlancho <carmenlancho@hotmail.com>
2025-10-14 12:50:59 -0600

    Función correlaciones, dist euclídea y clustering para datos originales

--- UnivariateInstanceComplexityAnalysis_Code.py
+++ UnivariateInstanceComplexityAnalysis_Code.py
@@ -19,7 +19,23 @@ from scipy.cluster.hierarchy import linkage, dendrogram
 import seaborn as sns
 import matplotlib.pyplot as plt
 import glob
-
+from sklearn.datasets import make_classification
+from sklearn.preprocessing import StandardScaler
+from sklearn.feature_selection import mutual_info_classif, f_classif
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegressionCV
+from sklearn.preprocessing import StandardScaler
+from skrebate import ReliefF
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+import xgboost as xgb
+from sklearn.metrics import accuracy_score, make_scorer
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import StratifiedKFold, cross_validate
+from sklearn.linear_model import LogisticRegression
 
 import os
 root_path = os.getcwd()
@@ -73,7 +89,6 @@ def analyze_variable_relationships(dataset_files, measures=["Hostility", "N1", "
             euclid_vals = dist_euclid.values[mask_offdiag]
 
             # Resúmenes
-            # --- Calcular estadísticas descriptivas ---
             summary_stats = {
                 "dataset": dataset_name,
                 "measure": measure,
@@ -166,4 +181,171 @@ def analyze_variable_relationships(dataset_files, measures=["Hostility", "N1", "
 # en el d23 se queda pillado
 
 
+
+# Función para generar datos sintéticos
+def generate_synthetic_dataset(n_samples, n_informative, n_noise,n_redundant_linear, n_redundant_nonlinear,
+                                flip_y, class_sep, n_clusters_per_class, weights, random_state=42, noise_std=0.05):
+    rng = np.random.RandomState(random_state)
+
+    # Generamos solo informativas + ruido
+    X, y = make_classification(
+        n_samples=n_samples,
+        n_features=n_informative + n_noise,
+        n_informative=n_informative,
+        n_redundant=0,
+        n_repeated=0,
+        flip_y = flip_y,
+        class_sep = class_sep,
+        n_clusters_per_class = n_clusters_per_class,
+        weights =weights,
+        shuffle=False,
+        random_state=random_state
+    )
+
+    X = preprocessing.scale(X)
+    df = pd.DataFrame(X, columns=[f"f{i}" for i in range(X.shape[1])])
+    formulas = {}
+    formulas_nonlinear = {}
+
+    # Redundantes lineales
+    for j in range(n_redundant_linear):
+        idx1, idx2 = rng.choice(n_informative, size=2, replace=False)
+        coef1, coef2 = rng.uniform(-2, 2, size=2)
+        new_name = f"f{df.shape[1]}"
+        new_feature = coef1*df[f"f{idx1}"] + coef2*df[f"f{idx2}"]
+        if noise_std > 0:
+            new_feature += rng.normal(0, noise_std, size=n_samples)
+        df[new_name] = new_feature
+        formulas[new_name] = f"{coef1:.2f}*f{idx1} + {coef2:.2f}*f{idx2}" + ("" if noise_std==0 else " + ruido")
+
+    # Redundantes no lineales
+    for j in range(n_redundant_nonlinear):
+        idx = rng.choice(n_informative, size=2, replace=False)
+        func = rng.choice([np.sin, np.cos, np.square, np.exp])
+        new_name = f"f{df.shape[1]}"
+        new_feature = func(df[f"f{idx[0]}"]) + df[f"f{idx[1]}"]
+        if noise_std > 0:
+            new_feature += rng.normal(0, noise_std, size=n_samples)
+        df[new_name] = new_feature
+        formulas_nonlinear[new_name] = f"{func.__name__}(f{idx[0]}) + f{idx[1]}" + ("" if noise_std==0 else " + ruido")
+
+    dict_info_feature = {
+        "informative": [f"f{i}" for i in range(n_informative)],
+        "noise": [f"f{i}" for i in range(n_informative, n_informative + n_noise)],
+        "redundant_linear": list(formulas.keys()),
+        "redundant_nonlinear": list(formulas_nonlinear.keys()),
+        "formulas_linear": formulas,
+        "formulas_nonlinear": formulas_nonlinear
+    }
+
+    return df, y, dict_info_feature
+
+
+
+
+
+
+## Misma función pero para datos originales, es decir, no depende de medidas de complejidad
+
+def analyze_variable_relationships_original_datasets(dataset_files,show_plots=False):
+    """
+    Analiza la relación entre variables para cada dataset
+
+   Calcula:
+        - Correlación Pearson
+        - Correlación Spearman
+        - Distancia Euclídea
+        - Dendrograma jerárquico (linkage=ward)
+    """
+
+    results_summary = []
+
+    for dataset_name, X in dataset_files.items():
+        print(X)
+
+        # Correlaciones
+        corr_pearson = X.corr(method="pearson")
+        corr_spearman = X.corr(method="spearman")
+
+        # Distancias euclídea
+        dist_euclid = pd.DataFrame(squareform(pdist(X.T, metric="euclidean")),
+            index=X.columns,columns=X.columns)
+
+        # Quitamos valores de la diagonal
+        mask_offdiag = ~np.eye(corr_pearson.shape[0], dtype=bool)
+
+        pearson_vals = corr_pearson.values[mask_offdiag]
+        spearman_vals = corr_spearman.values[mask_offdiag]
+        euclid_vals = dist_euclid.values[mask_offdiag]
+
+        # Resúmenes
+        summary_stats = {
+                "dataset": dataset_name,
+                "pearson_max": np.max(pearson_vals),
+                "pearson_min": np.min(pearson_vals),
+                "pearson_mean": np.mean(pearson_vals),
+                "pearson_median": np.median(pearson_vals),
+                "pearson_std": np.std(pearson_vals),
+
+                "spearman_max": np.max(spearman_vals),
+                "spearman_min": np.min(spearman_vals),
+                "spearman_mean": np.mean(spearman_vals),
+                "spearman_median": np.median(spearman_vals),
+                "spearman_std": np.std(spearman_vals),
+
+                "euclid_max": np.max(euclid_vals),
+                "euclid_min": np.min(euclid_vals),
+                "euclid_mean": np.mean(euclid_vals),
+                "euclid_median": np.median(euclid_vals),
+                "euclid_std": np.std(euclid_vals)}
+
+        results_summary.append(summary_stats)
+        # show_plots = True
+        if show_plots:
+            # máscara triangular inferior dejando diagonal
+            mask_triu = np.triu(np.ones_like(corr_pearson, dtype=bool), k=1)
+            # Heatmap y dendrograma
+            fig, axes = plt.subplots(2, 2, figsize=(12, 8))
+
+            sns.heatmap(corr_pearson, ax=axes[0,0], cmap="coolwarm", center=0, vmin=-1, vmax=1,
+                                               mask=mask_triu, square=True)
+            axes[0,0].set_title("Pearson Corr")
+
+            sns.heatmap(corr_spearman, ax=axes[0,1], cmap="coolwarm", center=0, vmin=-1, vmax=1,
+                                                           mask=mask_triu, square=True)
+            axes[0,1].set_title("Spearman Corr")
+
+            sns.heatmap(dist_euclid, ax=axes[1,0], cmap="viridis",mask=mask_triu, square=True)
+            axes[1,0].set_title("Euclidean Dist")
+
+            # Dendrograma (con dist euclídea)
+            Z = linkage(squareform(dist_euclid), method="ward")
+            dendrogram(Z, labels=X.columns, leaf_rotation=90, ax=axes[1,1])
+            axes[1,1].set_title("Hierarchical Clustering (Ward)")
+
+            plt.suptitle(f"{dataset_name}", fontsize=20)
+            plt.tight_layout()
+
+            # if save_figures:
+            #     plt.savefig(path_datasets / f"{dataset_name}_{measure}_relations_dendrogram.png", dpi=200)
+            plt.show()
+            # plt.close()
+    summary_df = pd.DataFrame(results_summary)
+
+    return summary_df
+
+
+# X1, y1, _ = generate_synthetic_dataset(n_samples=1000,n_informative=10,n_noise=2,
+#                                          n_redundant_linear=4,n_redundant_nonlinear=2,
+#                                         flip_y=0, class_sep = 1, n_clusters_per_class=1 , weights=[0.5],
+#                                                      random_state=0,noise_std=0.01)
+# X2, y2, _ = generate_synthetic_dataset(n_samples=1000,n_informative=10,n_noise=2,
+#                                          n_redundant_linear=4,n_redundant_nonlinear=2,
+#                                     flip_y=0, class_sep = 0.6, n_clusters_per_class=1 , weights=[0.5],
+#                                                      random_state=0,noise_std=0.01)
+#
+# dataset_files = {
+#     "ArtificialDataset1": X1,
+#     "ArtificialDataset2": X2,
+# }
 
