de7d4a1c7f70d8eb8edc43b97e740db8e11b8acd
carmenlancho <carmenlancho@hotmail.com>
2025-11-06 08:23:24 -0600

    código comentado con todo ejecutado

--- DistributedFS_Complexity.py
+++ DistributedFS_Complexity.py
@@ -721,73 +721,73 @@ n_replicas = 200
 
 
 
-#### Dataset 12
-dataset_name = 'ArtificialDataset12'
-X, y, dict_info_feature = generate_synthetic_dataset(n_samples=3000,n_informative=25,n_noise=30,
-                                         n_redundant_linear=30,n_redundant_nonlinear=30,
-                                        flip_y=0.2, class_sep=0.9, n_clusters_per_class=1, weights=[0.4],
-                                                     random_state=987,noise_std=0.5)
+# #### Dataset 12
+# dataset_name = 'ArtificialDataset12'
+# X, y, dict_info_feature = generate_synthetic_dataset(n_samples=3000,n_informative=25,n_noise=30,
+#                                          n_redundant_linear=30,n_redundant_nonlinear=30,
+#                                         flip_y=0.2, class_sep=0.9, n_clusters_per_class=1, weights=[0.4],
+#                                                      random_state=987,noise_std=0.5)
+#
+#
+# run_distributed_cv_multiple_models(X, y, dict_info_feature, dataset_name, models_dict,
+#                                 measures=["Hostility", "N1", "kDN"],
+#                                     cv_splits=5, n_replicas=n_replicas, random_state=0,
+#                                         path="Results_FS_Distributed_CV", save_csv=True)
+#
 
 
-run_distributed_cv_multiple_models(X, y, dict_info_feature, dataset_name, models_dict,
-                                measures=["Hostility", "N1", "kDN"],
-                                    cv_splits=5, n_replicas=n_replicas, random_state=0,
-                                        path="Results_FS_Distributed_CV", save_csv=True)
+# #### Dataset 14
+# dataset_name = 'ArtificialDataset14'
+# X, y, dict_info_feature = generate_synthetic_dataset(n_samples=3000,n_informative=30,n_noise=40,
+#                                          n_redundant_linear=30,n_redundant_nonlinear=40,
+#                                         flip_y=0.2, class_sep=0.6, n_clusters_per_class=2, weights=[0.3],
+#                                                      random_state=95,noise_std=0.5)
+#
+# run_distributed_cv_multiple_models(X, y, dict_info_feature, dataset_name, models_dict,
+#                                 measures=["Hostility", "N1", "kDN"],
+#                                     cv_splits=5, n_replicas=n_replicas, random_state=0,
+#                                         path="Results_FS_Distributed_CV", save_csv=True)
+#
+
+# #### Dataset 18
+# dataset_name = 'ArtificialDataset18'
+# X, y, dict_info_feature = generate_synthetic_dataset(n_samples=500,n_informative=70,n_noise=40,
+#                                          n_redundant_linear=40,n_redundant_nonlinear=40,
+#                                         flip_y=0.4, class_sep=0.8, n_clusters_per_class=2, weights=[0.2],
+#                                                      random_state=9462,noise_std=0.5)
+#
+# run_distributed_cv_multiple_models(X, y, dict_info_feature, dataset_name, models_dict,
+#                                 measures=["Hostility", "N1", "kDN"],
+#                                     cv_splits=5, n_replicas=n_replicas, random_state=0,
+#                                         path="Results_FS_Distributed_CV", save_csv=True)
 
 
-
-#### Dataset 14
-dataset_name = 'ArtificialDataset14'
-X, y, dict_info_feature = generate_synthetic_dataset(n_samples=3000,n_informative=30,n_noise=40,
-                                         n_redundant_linear=30,n_redundant_nonlinear=40,
-                                        flip_y=0.2, class_sep=0.6, n_clusters_per_class=2, weights=[0.3],
-                                                     random_state=95,noise_std=0.5)
-
-run_distributed_cv_multiple_models(X, y, dict_info_feature, dataset_name, models_dict,
-                                measures=["Hostility", "N1", "kDN"],
-                                    cv_splits=5, n_replicas=n_replicas, random_state=0,
-                                        path="Results_FS_Distributed_CV", save_csv=True)
-
-
-#### Dataset 18
-dataset_name = 'ArtificialDataset18'
-X, y, dict_info_feature = generate_synthetic_dataset(n_samples=500,n_informative=70,n_noise=40,
-                                         n_redundant_linear=40,n_redundant_nonlinear=40,
-                                        flip_y=0.4, class_sep=0.8, n_clusters_per_class=2, weights=[0.2],
-                                                     random_state=9462,noise_std=0.5)
-
-run_distributed_cv_multiple_models(X, y, dict_info_feature, dataset_name, models_dict,
-                                measures=["Hostility", "N1", "kDN"],
-                                    cv_splits=5, n_replicas=n_replicas, random_state=0,
-                                        path="Results_FS_Distributed_CV", save_csv=True)
+# #### Dataset 20
+# dataset_name = 'ArtificialDataset20'
+# X, y, dict_info_feature = generate_synthetic_dataset(n_samples=500,n_informative=300,n_noise=60,
+#                                          n_redundant_linear=60,n_redundant_nonlinear=60,
+#                                         flip_y=0.1, class_sep=0.6, n_clusters_per_class=1, weights=[0.3],
+#                                                      random_state=4556,noise_std=0.5)
+#
+# run_distributed_cv_multiple_models(X, y, dict_info_feature, dataset_name, models_dict,
+#                                 measures=["Hostility", "N1", "kDN"],
+#                                     cv_splits=5, n_replicas=n_replicas, random_state=0,
+#                                         path="Results_FS_Distributed_CV", save_csv=True)
+#
+# #### Dataset 21
+# dataset_name = 'ArtificialDataset21'
+# X, y, dict_info_feature = generate_synthetic_dataset(n_samples=1000,n_informative=300,n_noise=100,
+#                                          n_redundant_linear=100,n_redundant_nonlinear=100,
+#                                         flip_y=0.1, class_sep=0.7, n_clusters_per_class=2, weights=[0.4],
+#                                                      random_state=996,noise_std=0.5)
+#
+# run_distributed_cv_multiple_models(X, y, dict_info_feature, dataset_name, models_dict,
+#                                 measures=["Hostility", "N1", "kDN"],
+#                                     cv_splits=5, n_replicas=n_replicas, random_state=0,
+#                                         path="Results_FS_Distributed_CV", save_csv=True)
 
 
-#### Dataset 20
-dataset_name = 'ArtificialDataset20'
-X, y, dict_info_feature = generate_synthetic_dataset(n_samples=500,n_informative=300,n_noise=60,
-                                         n_redundant_linear=60,n_redundant_nonlinear=60,
-                                        flip_y=0.1, class_sep=0.6, n_clusters_per_class=1, weights=[0.3],
-                                                     random_state=4556,noise_std=0.5)
-
-run_distributed_cv_multiple_models(X, y, dict_info_feature, dataset_name, models_dict,
-                                measures=["Hostility", "N1", "kDN"],
-                                    cv_splits=5, n_replicas=n_replicas, random_state=0,
-                                        path="Results_FS_Distributed_CV", save_csv=True)
-
-#### Dataset 21
-dataset_name = 'ArtificialDataset21'
-X, y, dict_info_feature = generate_synthetic_dataset(n_samples=1000,n_informative=300,n_noise=100,
-                                         n_redundant_linear=100,n_redundant_nonlinear=100,
-                                        flip_y=0.1, class_sep=0.7, n_clusters_per_class=2, weights=[0.4],
-                                                     random_state=996,noise_std=0.5)
-
-run_distributed_cv_multiple_models(X, y, dict_info_feature, dataset_name, models_dict,
-                                measures=["Hostility", "N1", "kDN"],
-                                    cv_splits=5, n_replicas=n_replicas, random_state=0,
-                                        path="Results_FS_Distributed_CV", save_csv=True)
-
-
-
+# Number of distinct clusters (144) found smaller than n_clusters (160). Possibly due to duplicate points in X.
 
 
 
